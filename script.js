3a1f46516b068932d61b9c340d824279d898ad21
Monaruku <yikxiangwong@gmail.com>
2025-02-27 14:26:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if (isMobile) {
+        document.querySelector(".facebook").addEventListener("click", function() {
+            window.open("fb://page/110600357296339", "_blank");
+        });
+    }
+    else {
+            document.querySelector(".facebook").addEventListener("click", function() {
+    window.open("htps://facebook.com", "_blank");
+    });
+    }
+
+
+
     document.querySelectorAll('.wifi').forEach(button => {
         button.addEventListener('click', function() {
             alert('This will redirect to the respective page wifi!');
         });
     });
 
-    document.querySelector(".facebook").addEventListener("click", function() {
-    window.open("fb://page/110600357296339", "_blank");
-    });
+
 });
